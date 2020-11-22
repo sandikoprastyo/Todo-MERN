5ae8b94e529de995778673235ce02de4695f59ba
AjiHidayat <hidayataji71@gmail.com>
2020-11-22 10:45:13 +0700

    tambah input bar

--- frontend/src/FormInput/FormInput.js
+++ frontend/src/FormInput/FormInput.js
@@ -3,20 +3,26 @@ import "./FormInput.css";
 import Button from "../Button/Button";
 
 const FormInput = (props) => {
-  const handleAdd = () => {
-    alert("Create List Success");
-  };
+
   return (
     <div className="container-hero">
       <div className="hero">
         <h2>
-          If you bussy <span>like a Boss, </span><span>Manage</span> your daily activity with
+          If you bussy <span>like a Boss, </span>
+          <span>Manage</span> your daily activity with
           <span id="box">iDO</span>
         </h2>
       </div>
       <form>
-        <textarea placeholder="Input todo:"></textarea>
-        <Button onClick={handleAdd} className="btn btn-add" name="Create" />
+        <div>
+          <label>Todo</label>
+          <textarea onChange={props.onChange} value={props.value} name="todo" type="text" placeholder="todo" required></textarea>
+        </div>
+        <div>
+          <label>Date</label>
+          <input onChange={props.onChange} value={props.value} name="date" type="text" placeholder="ex: 12-Desember-2020" required />
+        </div>
+        <Button className="btn btn-add" name="Create" />
       </form>
     </div>
   );
